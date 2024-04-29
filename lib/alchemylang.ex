8215663f27f30492b0305ce3944c6f9f3897edfe
signal86 <diafakemail@gmail.com>
2024-04-29 08:59:14 -0500

    error system rework

--- lib/alchemylang.ex
+++ lib/alchemylang.ex
@@ -15,6 +15,13 @@ defmodule Alchemylang do
   def run(fileName) do
     if File.exists?(fileName) do
       IO.puts "file found"
+      valid =
+        with {:ok} <- Compiler.compile(fileName),
+        do: {:ok}
+      case valid do
+        {:ok} -> IO.puts "compilation successful"
+        {:error, reason} -> IO.puts "build failed -> " |> Kernel.<>(reason)
+      end
       Compiler.compile(fileName)
     else
       IO.puts "file not found"
--- lib/compiler.ex
+++ lib/compiler.ex
@@ -6,16 +6,25 @@ defmodule Compiler do
     #   if (String.length(i) != 0) do i end
     # end)
     valid =
-      with {:ok} <- syntax(data),
+      with {:ok} <- data = syntax(data),
       do: {:ok}
     case valid do
-      {:ok} -> IO.puts "all good"
-      {:error, reason} -> IO.puts "build failed -> " |> Kernel.<>(reason)
+      {:ok} -> {:ok}
+      {:error, reason} -> {:error, reason}
     end
   end
 
   defp syntax(data) do
-    reason = "lol"
-    {:error, reason}
+    # reason = "lol"
+    # {:error, reason}
+    try do
+      data = Enum.map(data, fn i ->
+        reason = "test"
+        throw({:error, reason})
+      end)
+      data
+    catch
+      {:error, reason} -> {:error, reason}
+    end
   end
 end
