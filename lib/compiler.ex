8b27372341daf35b4fc9f1d7e36c6a28b4edd518
signal86 <diafakemail@gmail.com>
2024-05-03 13:08:18 -0500

    reserved keywords & 3d array

--- lib/compiler.ex
+++ lib/compiler.ex
@@ -17,19 +17,56 @@ defmodule Compiler do
     # Enum.map(data, fn i -> IO.puts i end)
   end
 
+  # syntax checking & lexizing
   defp lexer(data) do
     # reason = "lol"
     # {:error, reason}
     try do
-      newData = []
-      lineNum = 1
-      data = Enum.map(data, fn line ->
-        a = String.split(line, "")
-        newData
+      newData = [] # newData is the lexized/tokenized data
+      # lines
+      lineNum = 0
+      Enum.map(data, fn line ->
+        wordList = String.split(line, " ")
+        # individual words
+        wordNum = 0
+        Enum.map(wordList, fn keyword ->
+          # Reserved keywords
+          # global, var, const, signal, meta, view, create, redirect, on, end, if
+          # DATA STRUCTURE of returned data
+          # 3 dimensional arrays ?! !!? [[line, [tokens]], [line, [tokens]]] -> etc
+          case keyword do
+            "global" ->
+              # logic
+            "var" ->
+              # logic
+            "const" ->
+              # logic
+            "signal" ->
+              # logic
+            "meta" ->
+              # logic
+            "view" ->
+              # logic
+            "create" ->
+              # logic
+            "redirect" ->
+              # logic
+            "on" ->
+              # logic
+            "end" ->
+              # logic
+            "if" ->
+              # logic
+          end
+          wordNum = wordNum + 1
+        end)
+        # newData
         # reason = "test"
         # throw({:error, "syntax error on main.alc:1 -> \"creat text\" -> symbol not found"})
+        # error line = lineNum + 1
         lineNum = lineNum + 1
       end)
+      data = newData
       {:ok, data}
     catch
       {:error, reason} -> {:error, reason}
